7e4bfac2038eb964f2a504a81a18c05f8d5b40a2
Mubarak <mudet04@gmail.com>
2023-11-03 12:52:58 +0100

    #

--- src/config.js
+++ src/config.js
@@ -1,46 +1,46 @@
 export const framerMotionConfig = {
-	type: "spring",
-	mass: 5,
-	stiffness: 500,
-	damping: 50,
-	restDelta: 0.0001,
+  type: "spring",
+  mass: 5,
+  stiffness: 500,
+  damping: 50,
+  restDelta: 0.0001,
 };
 
 export const skills = [
-	{
-		title: "HTML + CSS",
-		level: 90,
-	},
-	{
-		title: "JavaScript",
-		level: 78,
-	},
-	{
-		title: "ReactJS",
-		level: 65,
-	},
+  {
+    title: "HTML + CSS",
+    level: 90,
+  },
+  {
+    title: "JavaScript",
+    level: 78,
+  },
+  {
+    title: "ReactJS",
+    level: 65,
+  },
 ];
 
 export const projects = [
-	{
-		title: "Customize a Shirt",
-		demoUrl: "https://customize-shirt.netlify.app/",
-		gitUrl: "https://github.com/blackingg/threejs",
-		image: "img/Customize-Shirt.png",
-		description: "Recreating the Atmos Awwwards website with React Three Fiber",
-	},
-	{
-		title: "Pokedex",
-		demoUrl: "https://pokedex-blackingg.netlify.app/",
-		gitUrl: "",
-		image: "img/Pokedex.png",
-		description: "A Pokedex website",
-	},
-	{
-		title: "Kanagame",
-		demoUrl: "",
-		gitUrl: "",
-		image: "img/Customize-Shirt.png",
-		description: "Learn how to use ReadyPlayerMe to create a 3D avatar",
-	},
+  {
+    title: "Customize a Shirt",
+    demoUrl: "https://customize-shirt.netlify.app/",
+    gitUrl: "https://github.com/blackingg/threejs",
+    image: "img/Customize-Shirt.png",
+    description: "Recreating the Atmos Awwwards website with React Three Fiber",
+  },
+  {
+    title: "Pokedex",
+    demoUrl: "https://pokedex-blackingg.netlify.app/",
+    gitUrl: "https://github.com/blackingg/pokemon_app",
+    image: "img/Pokedex.png",
+    description: "A Pokedex website",
+  },
+  {
+    title: "Kanagame",
+    demoUrl: "",
+    gitUrl: "",
+    image: "img/Customize-Shirt.png",
+    description: "Learn how to use ReadyPlayerMe to create a 3D avatar",
+  },
 ];
