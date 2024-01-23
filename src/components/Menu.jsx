dd0ba24c8a6f1ccf7968ad8b08799aa8d4931649
Mubarak <mudet04@gmail.com>
2024-01-23 22:40:14 +0100

    socialsUpdate

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,102 +6,104 @@ import { AiOutlineGithub, AiFillLinkedin } from "react-icons/ai";
 import yourAudioFile from "../assets/audio.mp3";
 
 export const Menu = (props) => {
-	const { onSectionChange, menuOpened, setMenuOpened, isLoading } = props;
-	const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const { onSectionChange, menuOpened, setMenuOpened, isLoading } = props;
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
 
-	const handleAudioButtonClick = () => {
-		setIsAudioPlaying(!isAudioPlaying);
-	};
-	return (
-		<>
-			<button
-				onClick={() => setMenuOpened(!menuOpened)}
-				className="z-20 fixed top-12 right-12 p-3 bg-[#71a71a] w-11 h-11 rounded-md"
-			>
-				<div
-					className={`bg-white h-0.5 rounded-md w-full transition-all ${
-						menuOpened ? "rotate-45  translate-y-0.5" : ""
-					}`}
-				/>
-				<div
-					className={`bg-white h-0.5 rounded-md w-full my-1 ${
-						menuOpened ? "hidden" : ""
-					}`}
-				/>
-				<div
-					className={`bg-white h-0.5 rounded-md w-full transition-all ${
-						menuOpened ? "-rotate-45" : ""
-					}`}
-				/>
-			</button>
-			<div
-				className={`z-10 fixed top-0 right-0 bottom-0 bg-white transition-all overflow-hidden flex flex-col
+  const handleAudioButtonClick = () => {
+    setIsAudioPlaying(!isAudioPlaying);
+  };
+  return (
+    <>
+      <button
+        onClick={() => setMenuOpened(!menuOpened)}
+        className="z-20 fixed top-12 right-12 p-3 bg-[#71a71a] w-11 h-11 rounded-md"
+      >
+        <div
+          className={`bg-white h-0.5 rounded-md w-full transition-all ${
+            menuOpened ? "rotate-45  translate-y-0.5" : ""
+          }`}
+        />
+        <div
+          className={`bg-white h-0.5 rounded-md w-full my-1 ${
+            menuOpened ? "hidden" : ""
+          }`}
+        />
+        <div
+          className={`bg-white h-0.5 rounded-md w-full transition-all ${
+            menuOpened ? "-rotate-45" : ""
+          }`}
+        />
+      </button>
+      <div
+        className={`z-10 fixed top-0 right-0 bottom-0 bg-white transition-all overflow-hidden flex flex-col
       ${menuOpened ? "w-80" : "w-0"}`}
-			>
-				<div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
-					<>
-						<MenuButton
-							label="Home"
-							onClick={() => onSectionChange(0)}
-						/>
-						<MenuButton
-							label="About"
-							onClick={() => onSectionChange(1)}
-						/>
-						<MenuButton
-							label="Projects"
-							onClick={() => onSectionChange(2)}
-						/>
-						<MenuButton
-							label="Contact"
-							onClick={() => onSectionChange(3)}
-						/>
-					</>
-				</div>
-				<div className=" end-0 pb-10 text-xl flex items-start justify-center gap-6 cursor-pointer ">
-					<a
-						className="socials"
-						href="#"
-					>
-						<AiOutlineGithub className=" hover:text-[#436112] transition-colors" />
-					</a>
-					<a
-						className="socials"
-						href="#"
-					>
-						<AiFillLinkedin className=" hover:text-[#436112] transition-colors" />
-					</a>
-				</div>
-			</div>
+      >
+        <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
+          <>
+            <MenuButton
+              label="Home"
+              onClick={() => onSectionChange(0)}
+            />
+            <MenuButton
+              label="About"
+              onClick={() => onSectionChange(1)}
+            />
+            <MenuButton
+              label="Projects"
+              onClick={() => onSectionChange(2)}
+            />
+            <MenuButton
+              label="Contact"
+              onClick={() => onSectionChange(3)}
+            />
+          </>
+        </div>
+        <div className=" end-0 pb-10 text-xl flex items-start justify-center gap-6 cursor-pointer ">
+          <a
+            className="socials"
+            href="https://github.com/blackingg"
+            target="_blank"
+          >
+            <AiOutlineGithub className=" hover:text-[#436112] transition-colors" />
+          </a>
+          <a
+            className="socials"
+            href="https://www.linkedin.com/in/mubarak-odetunde-258494236/"
+            target="_blank"
+          >
+            <AiFillLinkedin className=" hover:text-[#436112] transition-colors" />
+          </a>
+        </div>
+      </div>
 
-			<button
-				className={`z-20 fixed top-12 right-[6.5rem] p-3 w-11 h-11 rounded-md first-letterfirst-line text-white
+      <button
+        className={`z-20 fixed top-12 right-[6.5rem] p-3 w-11 h-11 rounded-md first-letterfirst-line text-white
 				${isAudioPlaying ? "bg-[#71a71a]  " : " bg-[#a8aaa5]"}
 				`}
-				onClick={handleAudioButtonClick}
-			>
-				{isAudioPlaying ? <HiSpeakerWave /> : <HiSpeakerXMark color="white" />}
-			</button>
-			{isAudioPlaying && (
-				<audio autoPlay>
-					<source
-						src={yourAudioFile}
-						type="audio/mp3"
-					/>
-				</audio>
-			)}
-		</>
-	);
+        onClick={handleAudioButtonClick}
+      >
+        {isAudioPlaying ? <HiSpeakerWave /> : <HiSpeakerXMark color="white" />}
+      </button>
+      {isAudioPlaying && (
+        <audio autoPlay>
+          <source
+            src={yourAudioFile}
+            type="audio/mp3"
+          />
+        </audio>
+      )}
+    </>
+  );
 };
 
 const MenuButton = (props) => {
-	const { label, onClick } = props;
-	return (
-		<button
-			onClick={onClick}
-			className="text-2xl font-bold cursor-pointer hover:text-[#436112] transition-colors"
-		>
-			{label}
-		</button>
-	);
+  const { label, onClick } = props;
+  return (
+    <button
+      onClick={onClick}
+      className="text-2xl font-bold cursor-pointer hover:text-[#436112] transition-colors"
+    >
+      {label}
+    </button>
+  );
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,75 +10,75 @@ import { AiOutlineLink, AiFillGithub } from "react-icons/ai";
 import "../index.css";
 
 export default function Projects() {
-	return (
-		<div className="flex flex-col w-5/6 lg:w-full h-full gap-8 ">
-			<h2 className="text-4xl md:text-5xl font-bold text-[#354719]">
-				Projects
-			</h2>
-			<Swiper
-				effect={"coverflow"}
-				rewind={true}
-				slidesPerView={1}
-				spaceBetween={10}
-				loop={false}
-				coverflowEffect={{
-					rotate: 50,
-					stretch: 0,
-					depth: 100,
-					modifier: 1,
-					slideShadows: true,
-				}}
-				navigation={true}
-				modules={[EffectCoverflow, Navigation]}
-				className="w-full ml-7 md:ml-auto md:p-20  lg:w-[56rem]"
-			>
-				{...projects.map((project, index) => (
-					<SwiperSlide
-						key={project}
-						className="lg:snap-center text-center  lg:space-y-5 mt-20 lg:mt-0 pt-5 md:pb-10  px-5 sm:h-screen md:h-[23rem] lg:h-[30rem]  bg-[#abb897]"
-					>
-						<div className="text-white text-3xl lg:text-6xl font-bold px-10">
-							{project.title}
-						</div>
-						<img
-							width={400}
-							className="self-center m-auto border-solid border-2 border-[#526635]"
-							src={project.image}
-						/>
-						<div className="space-y-[-35px] lg:space-y-[-35px] pb-5">
-							<div>{project.description}</div>
-							<div className="space-x-4">
-								<a
-									href={project.demoUrl}
-									target="_blank"
-								>
-									<button
-										className={`bg-[#6d8a41] hover:bg-[#394b1e] py-4 px-8 rounded-lg font-bold text-lg mt-16 ${
-											!project.demoUrl ? "cursor-not-allowed" : ""
-										}`}
-										disabled={!project.demoUrl}
-									>
-										<AiOutlineLink color="white" />
-									</button>
-								</a>
-								<a
-									href={project.gitUrl}
-									target="_blank"
-								>
-									<button
-										className={`bg-[#6d8a41] hover:bg-[#394b1e] py-4 px-8 rounded-lg font-bold text-lg mt-16 ${
-											!project.gitUrl ? "cursor-not-allowed" : ""
-										}`}
-										disabled={!project.gitUrl}
-									>
-										<AiFillGithub color="white" />
-									</button>
-								</a>
-							</div>
-						</div>
-					</SwiperSlide>
-				))}
-			</Swiper>
-		</div>
-	);
+  return (
+    <div className="flex flex-col w-5/6 lg:w-full h-full gap-8 ">
+      <h2 className="text-4xl md:text-5xl font-bold text-[#354719]">
+        Projects
+      </h2>
+      <Swiper
+        effect={"coverflow"}
+        rewind={true}
+        slidesPerView={1}
+        spaceBetween={10}
+        loop={false}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        navigation={true}
+        modules={[EffectCoverflow, Navigation]}
+        className="w-full ml-7 md:ml-auto md:p-20 lg:py-20 lg:px-02 lg:w-[56rem]"
+      >
+        {...projects.map((project, index) => (
+          <SwiperSlide
+            key={project}
+            className="lg:snap-center text-center  lg:space-y-5 mt-20 lg:mt-0 pt-5 md:pb-10  px-5 sm:h-screen md:h-[23rem] lg:h-[30rem]  bg-[#abb897]"
+          >
+            <div className="text-white text-3xl lg:text-6xl font-bold px-10">
+              {project.title}
+            </div>
+            <img
+              width={400}
+              className="self-center m-auto border-solid border-2 border-[#526635]"
+              src={project.image}
+            />
+            <div className="space-y-[-35px] lg:space-y-[-35px] pb-5">
+              <div>{project.description}</div>
+              <div className="space-x-4">
+                <a
+                  href={project.demoUrl}
+                  target="_blank"
+                >
+                  <button
+                    className={`bg-[#6d8a41] hover:bg-[#394b1e] py-4 px-8 rounded-lg font-bold text-lg mt-16 ${
+                      !project.demoUrl ? "cursor-not-allowed" : ""
+                    }`}
+                    disabled={!project.demoUrl}
+                  >
+                    <AiOutlineLink color="white" />
+                  </button>
+                </a>
+                <a
+                  href={project.gitUrl}
+                  target="_blank"
+                >
+                  <button
+                    className={`bg-[#6d8a41] hover:bg-[#394b1e] py-4 px-8 rounded-lg font-bold text-lg mt-16 ${
+                      !project.gitUrl ? "cursor-not-allowed" : ""
+                    }`}
+                    disabled={!project.gitUrl}
+                  >
+                    <AiFillGithub color="white" />
+                  </button>
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
